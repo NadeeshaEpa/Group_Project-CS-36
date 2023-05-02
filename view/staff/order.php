f7e5761aceb9b3985dea875be90b08f200d48506
Tharindi Senadeera <tia.tharindi@gmail.com>
2023-05-02 12:21:05 +0530

    final

--- view/staff/order.php
+++ view/staff/order.php
@@ -177,19 +177,19 @@ require_once("../../config.php");?>
                  <td>'.$amount.'</td>' ;
 				
 				if($status==1){
-					echo'<td>Delivered</td>';
+					echo'<td style="color:green;">Delivered</td>';
 				}
 				else if($status==0){
-					echo'<td>On the way</td>';
+					echo'<td style="color:purple;">On the way</td>';
 				}
 				else if($status==NULL){
-					echo'<td>Not assigned</td>';
+					echo'<td style="color:red;">Not assigned</td>';
 				}
 				else if($status==3){
-					echo'<td>Courier service</td>';
+					echo'<td style="color:orange;">Courier service</td>';
 				}
 				else if($status==4){
-					echo'<td>Picked</td>';
+					echo'<td style="color:blue;">Picked</td>';
 				}
 				else{
 					echo'<td>No delivery</td>';
