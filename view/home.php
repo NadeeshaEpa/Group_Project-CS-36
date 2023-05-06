c3d20f22066462e9733f6864e085441e6fcdf982
ganganth <2020cs058@stu.ucsc.cmb.ac.lk>
2023-05-06 17:58:42 +0530

    Add mobile responsive

--- view/home.php
+++ view/home.php
@@ -4,6 +4,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="http://localhost/Group_36/public/css/customer/home.css">
+    <link rel="stylesheet" href="http://localhost/Group_36/public/css/home_responsive.css">
     <title>FAGO</title>
     <link rel="icon" type="image/png" href="http://localhost/Group_36/public/images/logo.png">  
     
